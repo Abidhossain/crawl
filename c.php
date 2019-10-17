6bfed020acd0ae190296b5388a15189f660e171d
Abidhossain <mohammad.abid.dev@gmail.com>
2019-10-18 03:16:03 +0600

    data crawling

--- c.php
+++ c.php
@@ -18,4 +18,19 @@
  	$get_content = explode('<title>',$contents);
  	$get_title = explode('</title>',$get_content[1]);
  	print_r($get_title[0]);
+
+
+ // 	$url ="http://bnb2.smartsoft-bd.com/food-item/dark-chocolate-biscuit-200gm";
+	// $ch = curl_init();
+	// curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+	// curl_setopt($ch, CURLOPT_HEADER,0);
+	// curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.7) Gecko/20091221 Firefox/3.5.7 GTB6 (.NET CLR 3.0.4506.2152)");
+	// curl_setopt($ch, CURLOPT_FOLLOWLOCATION,1);
+	// curl_setopt($ch, CURLOPT_URL, $url);	
+	// $content = curl_exec($ch);
+	// curl_close($ch); 
+ 
+ // 	$get_content = explode('<title>',$content);
+ // 	$get_title = explode('</title>',$get_content[1]);
+ // 	print_r($get_title[0]);
 ?>
